8c380b34318d384be5ed735bd7e61b1fa01fe42d
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2026-05-19 21:33:17 +0600

    add main server

---
+++ main.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { run } from './src/services/db.js';
+import router from './src/router/index.js';
+
+const app = express();
+const PORT = 3001;
+
+
+
+app.use(express.json());
+app.use('/', router)
+
+const startServer = async () => {
+    try {
+        await run();
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+app.listen(PORT, () => {
+    startServer();
+    console.log(`Server listening on port ${PORT}`);
+});
+
+export default app;
