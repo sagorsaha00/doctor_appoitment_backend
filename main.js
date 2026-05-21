d017f18570c84b378b154dab1a3f2ce49930fcfb
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2026-05-21 20:51:52 +0600

    put method add

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import { run } from './src/services/db.js';
 import router from './src/router/index.js';
 
 const app = express();
 const PORT = 3001;
-
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 
 
 app.use(express.json());
